121eca7d9a4dec56ad724d55b57693fefb8a106e
Rafael José do Nascimento <rafaeljosev10@gmail.com>
2020-12-02 22:53:41 -0300

    BS4 + WP #06

--- wordpress/wp-content/themes/bs4wp/index.php
+++ wordpress/wp-content/themes/bs4wp/index.php
@@ -1,38 +1,45 @@
 <?php get_header()?>
 
-    <div class="row">
-      <div class="col-lg-8 col-md-8 col-sm-12">
+<div class="row">
+  <div class="col-lg-8 col-md-8 col-sm-12">
 
-        <div class="blog-post">
-          <h3 class="mb-3 pb-2 border-bottom"><a href="#">Lorem ipsum dolor sit amet</a></h3>
-          <div class="row">
-            <div class="col-md-12 col-lg-6 mb-3">
-              <a href="#">
-                <img src="assets/img.png" class="img-fluid rounded" alt="gato">
-              </a>
-            </div>
-            <div class="col-md-12 col-lg-6 mb-3">
-              <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio hic illo perspiciatis consequatur
-                error accusamus rem corrupti, inventore tenetur aliquid ex laboriosam vel, voluptatibus dolores dolorem
-                ad,
-                odit maiores eius quibusdam ipsam quos sit culpa numquam eum! Voluptate, rem? Consectetur at cumque
-                accusantium? Adipisci temporibus ipsum dolorum? Impedit, nam aspernatur.</p>
-            </div>
-          </div>
-          <p class="text-muted">Publicado em <span class="badge badge-my-color-4">23/07/2018</span></p>
+    <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+    <div class="blog-post">
+      <h3 class="mb-3 pb-2 border-bottom"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+      <div class="row">
+        <div class="col-md-12 col-lg-6 mb-3">
+          <a href="<?php the_permalink(); ?>">
+          <?php the_post_thumbnail('post-thumbnail', array('class' => 'img-fluid')) ?>
+            
+          </a>
         </div>
-
-        <div class="blog-pagination mb-5">
-          <a href="#" class="btn btn-outline-my-color-5">Mais antigos</a>
-          <a href="#" class="btn btn-outline-my-color-5">Mais novos</a>
+        <div class="col-md-12 col-lg-6 mb-3">
+          <?php the_excerpt(); ?>
         </div>
-
       </div>
+      <p class="text-muted">Publicado em 
+        <span class="badge badge-my-color-4">
+        <?php echo get_the_date('d/m/y'); ?>
+        </span>
+      </p>
+    </div>
 
-      <?php get_sidebar() ?>
+    <?php endwhile; ?>
 
+    <?php else : get_404_teplate(); endif;?>
+
+    <div class="blog-pagination mb-5">
+      <?php next_posts_link(' Mais antigos'); ?>
+      <?php previous_posts_link(' Mais novos ') ?>
     </div>
 
   </div>
+
+  <?php get_sidebar() ?>
+
+</div>
+
+</div>
 
 <?php get_footer() ?>
